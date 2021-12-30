836c789dbdb218c4a82389f1b5d7680b30fcaf97
Emily Curran <1303currane@gmail.com>
2021-12-30 04:54:37 +0000

    first iteration of working calculator

--- script.js
+++ script.js
@@ -1,7 +1,158 @@
 class Calculator {
-    constructor(){
+    userInput = "";
+    lastAnswer = ""; 
 
+    parseInput(inputString){
+        //parses the brackets
+        let indexLeftBracket;
+        let indexRightBracket;
+        for(let i=0; i<inputString.length; i++){
+           if(inputString[i] == "("){
+                indexLeftBracket = i;
+           } 
+           if(inputString[i] == ")"){
+               indexRightBracket = i;
+               let leftString = inputString.substring(0, indexLeftBracket);
+               let middleString = inputString.substring(indexLeftBracket+1, indexRightBracket);
+               let rightString = inputString.substring(indexRightBracket+1);
+               let ret =  this.parseInput(leftString + this.parseInput(middleString) + rightString);
+               return ret;
+           }
+        }
+
+        //parses the + and - signs
+        for(let i=0; i<inputString.length; i++){
+            if (inputString[i] == "+"){
+                let leftString = inputString.substring(0,i);
+                let rightString = inputString.substring(i+1);
+                let ret = this.parseInput(leftString) + this.parseInput(rightString);
+                return ret;
+            } 
+            else if(inputString[i] == "-"){
+                let leftString = inputString.substring(0,i);
+                let rightString = inputString.substring(i+1);
+                let ret = this.parseInput(leftString) - this.parseInput(rightString);
+                return ret;
+            }
+        }
+
+        //parses the * and / signs
+        for(let i=0; i<inputString.length; i++){
+            if (inputString[i] == "*"){
+                let leftString = inputString.substring(0,i);
+                let rightString = inputString.substring(i+1);
+                let ret = this.parseInput(leftString) * this.parseInput(rightString);
+                return ret;
+            } 
+            else if(inputString[i] == "/"){
+                let leftString = inputString.substring(0,i);
+                let rightString = inputString.substring(i+1);
+                let ret = this.parseInput(leftString) / this.parseInput(rightString);
+                return ret;
+            }
+        }
+
+        return parseFloat(inputString);
+    }
+
+    updateUserInput(text){
+        let temp = this.userInput+text;
+        if(temp.length<22){
+            this.userInput = temp;
+        }
+    }
+
+    formatAnswer(ans){
+        let ret = "";
+
+        if(ans == Infinity){
+            ret = "zero division error";
+            this.lastAnswer = "";
+        }else if(ans == "NaN"){
+            ret = "syntax error";
+            this.lastAnswer = "";
+        }else{
+            ret = parseFloat(ans.toFixed(5));
+            this.lastAnswer = ret;
+        }
+
+        return ret;
     }
 }
 
-console.log("hello world");
+myCalc = new Calculator();
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+const textButtons = Array.from(document.querySelectorAll('.string-input'));
+const ans = document.getElementById("answer");
+const equals = document.getElementById("equals");
+const clr = document.getElementById("clr");
+
+
+textButtons.forEach(textButton => textButton.addEventListener('click', function(){
+    text = this.innerHTML;
+    myCalc.updateUserInput(text);
+    document.getElementById("user-input").innerText = myCalc.userInput;
+}));
+
+equals.addEventListener('click', function(){
+    var ans = myCalc.parseInput(myCalc.userInput);
+    let output = myCalc.formatAnswer(ans);
+    document.getElementById("output").innerText = output;
+    myCalc.userInput = "";
+    document.getElementById("user-input").innerText = "";
+});
+
+clr.addEventListener('click', function(){
+    myCalc.userInput = "";
+    myCalc.lastAnswer = "";
+    document.getElementById("user-input").innerText = "";
+    document.getElementById("output").innerText = "";
+});
+
+ans.addEventListener('click', function(){
+        myCalc.updateUserInput(myCalc.lastAnswer);
+        document.getElementById("user-input").innerText = myCalc.userInput;
+});
+
+
+// css styling
+
+buttons.forEach(button => button.addEventListener('mouseover', function(){
+    color = this.getAttribute("data-color");
+    this.style.backgroundColor = color;
+    if(this.getAttribute("data-color") != 'white'){
+        this.style.color = "white";
+    }
+}));
+
+buttons.forEach(button => button.addEventListener('mouseout', function(){
+    color = this.getAttribute("data-color");
+    this.style.backgroundColor = "";
+    this.style.color = "rgb(151, 151, 151)";
+}));
+
+//adding keyboard functionality 
+
+document.addEventListener('keydown', function (event) {
+    //add key bindings for standard keys
+    for(let i=0; i<textButtons.length; i++){
+        if(event.key == textButtons[i].innerText){
+            textButtons[i].click();
+        }
+    }
+
+    if (event.key === 'Backspace') {
+        clr.click();
+    }
+
+    if (event.key === 'Enter' || event.key === '='){
+        equals.click();
+    }
+
+    if(event.key === 'a' ){
+        ans.click();
+    }
+
+});
+
